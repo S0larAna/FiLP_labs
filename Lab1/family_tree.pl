82e37c7b8d4ba8c03759846f4782e3c6049909b5
S0LARANA <s0169513@edu.kubsu.ru>
2024-03-19 12:24:31 +0300

    1.11 Add comments, family_tree.pl cleaned up

--- Lab1/family_tree.pl
+++ Lab1/family_tree.pl
@@ -48,56 +48,78 @@ parent(duhovlad,zlatomir).
 parent(zhdana,zdislava).
 parent(zhdana,zlatomir).
 
-
 men():- man(X), print(X), nl, fail.
 women():- woman(X), print(X), nl, fail.
 children(X):- parent(X,Y), print(Y), nl, fail.
 
+% Predicate mother(+X: atom, +Y: atom)
 % Построить предикат mother(X, Y), который проверяет, является ли X матерью Y.
 mother(X,Y):- woman(X), parent(X,Y).
+
+% Predicate mother(+X: atom)
 % Построить предикат, mother(X), который выводит маму X.
 mother(X):- mother(Y,X), print(Y), nl.
 
+% Predicate brothers(+X:atom, +Y:atom)
 % Построить предикат brother(X, Y), который проверяет, является ли X братом Y.
 brothers(X,Y):- parent(Z,X), parent(Z,Y), man(X), man(Y).
+
+% Predicate brothers(+X:atom)
 % Построить предикат brothers(X), который выводит всех братьев X.
 brothers(X):- parent(Z,X), parent(Z,Y), man(Y), woman(Z), X\==Y, print(Y), nl, fail.
 
+% Predicate b_s(+X:atom, +Y:atom)
 % Построить предикат b_s(X,Y), который проверяет, являются ли X и Y родными
   %братом и сестрой или братьями или сестрами.
 b_s(X,Y):- parent(Z,X), parent(Z,Y), woman(X), woman(Y), print(sisters).
 b_s(X,Y):- parent(Z,X), parent(Z,Y), man(X), man(Y), print(brothers).
 b_s(X,Y):- parent(Z,X), parent(Z,Y), ((man(X), woman(Y));(man(Y), woman(X))), print(siblings).
+
+% Predicate b_s(+X:atom)
 % Построить предикат b_s(X), который выводит
   %всех братьев или сестер X.
 b_s(X):- parent(Z,X), parent(Z,Y), woman(Z), X\==Y, print(Y), nl, fail.
 
 % задание 2.
+% Predicate son(+X:atom, +Y:atom)
 % Построить предикат son(X, Y), который проверяет, является ли X сыном Y.
 son(X,Y):- parent(Y,X), man(X).
-% Построить предикат, son(X), который выводит сына X.
-son(X):- parent(X,Y), man(Y), print(Y), nl, fail.
 
+% Predicate son(+X:atom)
+% Построить предикат, son(X), который выводит сына X.
+son(X):- son(Y, X), print(Y), nl, fail.
+
+% Predicate sisters(+X:atom, +Y:atom)
 % Построить предикат sister(X, Y), который проверяет, является ли X сестрой Y.
-sister(X,Y):- woman(X), parent(Z,X), parent(Z,Y), X\==Y.
+sister(X,Y):- woman(X), parent(Z,X), parent(Z,Y), woman(Z), X\==Y.
+
+% Predicate sisters(+X:atom)
 % Построить предикат sisters(X), который выводит всех сестер X.
-sisters(X):- woman(Y), parent(Z,X), parent(Z,Y), woman(Z), X\==Y, print(Y), nl, fail.
+sisters(X):- sister(Y, X), print(Y), nl, fail.
+
 
 % задание 3.
 % версия с использованием только базы фактов
+
+% Predicate grand_ma(+X:atom,+Y:atom)
 % Построить предикат grand_ma(X, Y), который проверяет, является ли X бабушкой Y.
 grand_ma(X,Y):- woman(X), parent(X,Z), parent(Z,Y).
-% Построить предикат grand_mas(X), который выводит всех бабушек X.
-grand_mas(X):- woman(Y), parent(Y,Z), parent(Z,X), print(Y), nl, fail.
 
+% Predicate grand_mas(+X:atom)
+% Построить предикат grand_mas(X), который выводит всех бабушек X.
+grand_mas(X):- grand_ma(Y, X), print(Y), nl, fail.
+
+% Predicate grand_pa_and_son(+X:atom,+Y:atom)
 % Построить предикат grand_pa_and_son(X,Y), который проверяет, являются ли X и Y
   %дедушкой и внуком или внуком и дедушкой.
 grand_pa_and_son(X,Y):- man(X), man(Y),(parent(X,Z), parent(Z,Y));(parent(Y,Z), parent(Z,X)).
 
 % Построить предикат, который проверяет, является ли X дядей Y. Построить
   %предикат, который выводит всех дядей X.
-uncle(X, Y):- man(X), parent(Z,Y), parent(A,Z), parent(A,X).
-uncles(X):- man(Y), parent(Z,X), parent(A,Z), parent(A,Y), woman(A), print(Y), nl, fail.
+  % Predicate uncle(+X:atom,+Y:atom)
+uncle(X, Y):- man(X), parent(Z,Y), parent(A,Z), parent(A,X), woman(A).
+% Predicate uncle(+X:atom)
+uncles(X):- uncle(Y,X), print(Y), nl, fail.
 
 % задание 3.
 % версия с использованием готовых предикатов.
