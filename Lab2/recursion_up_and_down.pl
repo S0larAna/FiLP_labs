71fb77adbd03fbbb7a0c806de7c58e6bcdd2e26e
S0LARANA <s0169513@edu.kubsu.ru>
2024-03-21 22:01:31 +0300

    2.2 Add recursion predicates, finished tasks 1, 2 and 3 Add read_list(-List), sum_list_down(+List, -Summ), sum_list_up(+List, -Sum), remove_items_with_digit_sum(+List, +Sum, -Result), max_digit_up(+N, -Max), max_digit(+N, -Max), sum_mul_3_up(+N, -Sum), sum_mul_3_down(+N, -Sum), count_divs_up(+N, -Count), count_divs_down(+N, -Count), min_el_ind(+[H|T], -MinInd), two_min_els(+List, -Min1, -Min2), count_even_els(+List, -Count)

--- Lab2/recursion_up_and_down.pl
+++ Lab2/recursion_up_and_down.pl
@@ -17,8 +17,7 @@ fact_down(N,_,N,_):-!,fail.
 fact_down(Y, Z, N, X) :- Y1 is Y + 1, Z1 is Z*Y1, fact_down(Y1, Z1, N, X).
 
 digit_sum(0,0):-!.
-digit_sum(X,Sum):-X1 is X // 10, Rem is X mod 10,
-	digit_sum(X,Sum):-X1 is X // 10, Rem is X mod 10, (X1, Sum1), Sum is Sum1 + Rem.
+digit_sum(X,Sum):-X1 is X // 10, Rem is X mod 10, digit_sum(X1, Sum1), Sum is Sum1 + Rem.
 
 digit_sum_down(X, Sum) :- digit_sum_down(X, 0, Sum).
 digit_sum_down(0, SumCur, SumCur) :- !.
@@ -30,22 +29,104 @@ free_of_squares(N, X) :- N *N =< X,(
                           free_of_squares(N1, X)
 ).
 
-read_str_f(A,N,Flag):-get0(X),r_str_f(X,A,[],N,0,Flag).
-r_str_f(-1,A,A,N,N,0):-!.
-r_str_f(10,A,A,N,N,1):-!.
-r_str_f(X,A,B,N,K,Flag):-K1 is K + 1,append(B,[X],B1),get0(X1),r_str_f(X1,A,B1,N,K1,Flag).
+read_list(List) :- read_list([], List).
 
-read_list_str(List,List_len):-read_str_f(A,N,Flag),r_l_s(List,List_len,[A],[N],Flag).
-r_l_s(List,List_len,List,List_len,0):-!.
-r_l_s(List,List_len,Cur_list,Cur_list_len,_):-
-	read_str_f(A,N,Flag),append(Cur_list,[A],C_l),append(Cur_list_len,[N],C_l_l),
-	r_l_s(List,List_len,C_l,C_l_l,Flag).
+read_list(Acc, List) :-
+    write('Enter an element (or press Enter to finish): '),
+    read_line_to_string(user_input, Input),
+    (   Input = ""
+    ->  reverse(Acc, List)
+    ;   (   atom_number(Input, Element)
+        ->  true
+        ;   Element = Input
+        ),
+        append(Acc, [Element], NewAcc),
+        read_list(NewAcc, List)
+    ).
 
+write_list([]) :- !.
+write_list([H|Tail]) :- write(H), nl, write_list(Tail).
 
+sum_list_down(List, Summ) :- sum_list_down(0, List, Summ).
+sum_list_down(Acc, [], Acc).
+sum_list_down(Acc, [H|Tail], Summ) :- NewAcc is Acc + H, sum_list_down(NewAcc, Tail, Summ).
 
+sum_list_up([], 0).
+sum_list_up([H|T], Sum) :- sum_list_up(T, AccSum), Sum is AccSum + H.
+
+remove_items_with_digit_sum([], _, []).
+remove_items_with_digit_sum([H|T], Sum, Result) :-
+    digit_sum(H, DigitSum),
+    (   DigitSum =:= Sum
+    ->  remove_items_with_digit_sum(T, Sum, Result)
+    ;   Result = [H|NewResult],
+        remove_items_with_digit_sum(T, Sum, NewResult)
+    ).
 
 
 % task 2
+max_digit_up(0, 0).
+max_digit_up(N, Max) :- CurDigit is N mod 10, N1 is N // 10, max_digit_up(N1, CurMax), (CurDigit>CurMax -> Max is CurDigit; Max is CurMax).
+
 max_digit(0, Max, Max) :- !.
 max_digit(N, Max) :- CurMax is N mod 10, N1 is N // 10, max_digit(N1, CurMax, Max).
-max_digit(N1, CurMax, Max) :- CurDigit is N1 mod 10, N is N1 // 10, (CurDigit > CurMax) -> NextMax is CurDigit, max_digit(N, NextMax, Max).
+max_digit(N1, CurMax, Max) :- CurDigit is N1 mod 10, N is N1 // 10, ((CurDigit > CurMax) -> NextMax is CurDigit; NextMax is CurMax), max_digit(N, NextMax, Max).
+
+sum_mul_3_up(0, 0).
+sum_mul_3_up(N, Sum) :- CurDigit is N mod 10, N1 is N // 10, sum_mul_3_up(N1, CurSum), (CurDigit mod 3 =:= 0 -> Sum is CurSum + CurDigit; Sum is CurSum).
+
+sum_mul_3_down(N, Sum) :- sum_mul_3_down(N, 0, Sum).
+sum_mul_3_down(0, CurSum, CurSum).
+sum_mul_3_down(N, CurSum, Sum) :- CurDigit is N mod 10, N1 is N // 10, (CurDigit mod 3 =:= 0 -> NextSum is CurSum + CurDigit; NextSum is CurSum),
+sum_mul_3_down(N1, NextSum, Sum).
+
+count_divs_up(N, 0, 1).
+count_divs_up(N, Count) :- Div is N, count_divs_up(N, CurCount, Div), (N mod Div =:= 0 -> Count is CurCount + 1; Count is CurCount).
+count_divs_up(N, Count, Div) :- NewDiv is Div - 1, count_divs_up(N, CurCount, NewDiv), (N mod NewDiv =:= 0 -> Count is CurCount + 1; Count is CurCount).
+
+count_divs_down(N, Count) :- count_divs_down(N, Count, 1, 1).
+count_divs_down(N, CurCount, N, CurCount).
+count_divs_down(N, Count, Div, CurCount) :- (N mod Div =:= 0 -> NextCount is CurCount + 1 ; NextCount is CurCount), NewDiv is Div + 1, count_divs_down(N, Count, NewDiv, NextCount).
+
+task1 :-
+    read_list(List),
+    write_list(List),
+    sum_list_down(List, Sum),
+    remove_items_with_digit_sum([123, 2, 3], 6, Result),
+    write(Result),
+    write(Sum).
+
+% task3 variant 2
+min_el_ind([H|T], MinInd) :- min_el_ind([H|T], 0, H, MinInd, 0).
+min_el_ind([], _, _, CurMinInd, CurMinInd).
+min_el_ind([H|T], CurInd, CurMin, MinInd, CurMinInd) :-
+    (H =< CurMin -> (NextMin is H, NextMinInd is CurInd);
+    NextMin is CurMin, NextMinInd is CurMinInd),
+    NextInd is CurInd + 1,
+    min_el_ind(T, NextInd, NextMin, MinInd, NextMinInd).
+
+two_min_els([A, B|T], Min1, Min2) :- T \== [], two_min_els(T, A, B, Min1, Min2).
+two_min_els([], CurMin1, CurMin2, CurMin1, CurMin2).
+two_min_els([H|T], CurMin1, CurMin2, Min1, Min2) :-
+    (H < CurMin1 ->
+    NextMin1 is H, NextMin2 is CurMin1;
+    (H < CurMin2 ->
+    NextMin1 is CurMin1, NextMin2 is H;
+    NextMin1 is CurMin1, NextMin2 is CurMin2)),
+    two_min_els(T, NextMin1, NextMin2, Min1, Min2).
+
+count_even_els(List, Count) :- count_even_els(List, 0, Count).
+count_even_els([], Count, Count).
+count_even_els([H|T], CurCount, Count) :-
+    (H mod 2 =:= 0 ->
+    NextCount is CurCount + 1;
+    NextCount is CurCount),
+    count_even_els(T, NextCount, Count).
+
+task3 :-
+    read_list(List),
+    min_el_ind(List, Ind),
+    write(Ind),
+    two_min_els(List, Min1, Min2),
+    write(Min1, Min2).
+
