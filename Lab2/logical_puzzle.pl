4d4ea95be9880b47c0492cd09934bde4d31b8e14
S0LARANA <s0169513@edu.kubsu.ru>
2024-03-22 02:18:58 +0300

    2.5 Add another logical puzzle summer_camp - solve logical puzzle: Пятеро детей Алик, Боря, Витя, Лена и Даша приехали в лагерь из 5 разных городов: Харькова, Умани, Полтавы, Славянска и Краматорска. Есть 4 высказывания: 1) Если Алик не из Умани, то Боря из Краматорска. 2) Или Боря, или Витя приехали из Харькова. 3) Если Витя не из Славянска, то Лена приехала из Харькова. 4) Или Даша приехала из Умани, или Лена из Краматорска. Кто откуда приехал?

--- Lab2/logical_puzzle.pl
+++ Lab2/logical_puzzle.pl
@@ -25,3 +25,36 @@ girlfriends:-
 
     Girlfriends = [[anya, AnyaDress, AnyaShoes], [valya, ValyaDress, ValyaShoes, [natasha, NatashaDress, NatashaShoes]]],
     write(Girlfriends).
+
+summer_camp :-
+    % Определяем список городов
+    Cities = [kharkiv, uman, poltava, slavyansk, kramatorsk],
+
+    member(Alik, Cities),
+    member(Borya, Cities),
+    member(Vitya, Cities),
+    member(Lena, Cities),
+    member(Dasha, Cities),
+
+    all_different([Alik, Borya, Vitya, Lena, Dasha]),
+
+    % Условие 1: Если Алик не из Умани, то Боря из Краматорска
+    (Alik \= uman ; Borya = kramatorsk),
+
+    % Условие 2: Или Боря, или Витя приехали из Харькова
+    (Borya = kharkiv ; Vitya = kharkiv),
+
+    % Условие 3: Если Витя не из Славянска, то Лена приехала из Харькова
+    (Vitya \= slavyansk ; Lena = kharkiv),
+
+    % Условие 4: Или Даша приехала из Умани, или Лена из Краматорска
+    (Dasha = uman ; Lena = kramatorsk),
+    Camp = [[alik, Alik], [borya, Borya], [vitya, Vitya], [lena, Lena], [dasha, Dasha]],
+    write(Camp).
+
+all_different([]).
+all_different([H|T]) :- member(H, T), !, fail.
+all_different([_|T]) :- all_different(T).
+
+
+
